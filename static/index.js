1af3645ee0ce7cc6ba3204ca40a8166fd6ffa10c
ChangSoo Choi <97ckdtn@gmail.com>
2023-03-15 15:05:52 +0900

    footer edit

--- static/index.js
+++ static/index.js
@@ -14,17 +14,16 @@ function listing() {
 		console.log(data)
 		let a = data['result']
 		$('#container').empty()// 프론트 변경에 따라 변경 필요
-        $('#footer').empty()// 프론트 변경에 따라 변경 필요
 		let name = a['name']
 		let age = a['age']
 		let gender = a['gender']
 		let mbti = a['mbti']
 		let tmi = a['tmi']
-		//let imglink=a['imglink']//별개이미지 사용
+		let imglink=a['imglink']//별개이미지 사용
         //let thumbnail = a['thumbnail'] // 메인페이지의 이미지를 사용
-		//let pro_con =a['pro_con']
-		//let goal_style = a['goal_style']
-		//let blog=a['blog']
+		let pro_con =a['pro_con']
+		let goal_style = a['goal_style']
+		let blog=a['blog']
 		//프론트 디자인 양식에 맞춰 수정필요
 		let temp_html = `
 			<div class="box_top">
@@ -34,18 +33,18 @@ function listing() {
 				</div>
 				<div class="mybox2">
 					<!--<img src="/static/image/googoo.jpeg" width="330px" height="300px" alt="">-->
-					<img src="${"사진"}" width="330px" height="300px" alt="">
+					<img src="${imglink}" width="330px" height="300px" alt="">
 				</div>
 			</div><!-- e:box_top -->
 			<div class="box_bot">
 				<div class="bot_left">
 					<div class="mybox5">
 						<h1>목표/협업스타일</h1>
-						${"목표"}
+						${goal_style}
 					</div>
 					<div class="mybox3">
 						<h1>장단점</h1>
-						${"장단"}
+						${pro_con}
 					</div>
 				</div><!-- e:bot_left -->
 			<div class="bot_right">
@@ -62,6 +61,7 @@ function listing() {
 			console.log(temp_html)
 
         
-            $('#footer>h3').text("블로그 주소")
+            $('#blogurl span').text(blog)
+			$("#blogurl").attr("href",blog)
 		})
 	}
